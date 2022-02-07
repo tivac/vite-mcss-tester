db8f74b804fd0dcff62cbaac34cd67234c38cadc
Pat Cavit <github@patcavit.com>
2022-02-07 15:05:21 -0800

    chore: removing dist code

--- dist/assets/other.03d031e7.js
+++
@@ -1,1 +0,0 @@
-import{b as c}from"./index.a826ac1e.js";const s=c+" mc4cd9cbe0_green";var n={green:s};console.log(n);const e=document.createElement("div");e.className=n.green;e.innerText="green";document.body.appendChild(e);
